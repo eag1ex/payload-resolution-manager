127cca75553b704591ee8fbb16f780ca414a80f0
eag1ex <eaglex@eaglex.net>
2019-12-29 11:36:05 +0700

    update async.example

---
+++ examples/app.async.data.js
@@ -0,0 +1,65 @@
+exports.assignONE = {
+    jobID: 'bank_1',
+    cusListA: [
+        {
+            uid: 'steve_jobs_12345',
+            accountName: 'Steve Jobbs',
+            bankName: 'Swiss Bank',
+            accountNumber: '2346547435',
+            accountType: 'investment',
+            'SWIFT_BIC': '34575',
+            portfolio: 20000000000
+        }, // _RI = 0
+        {
+            uid: 'bill_gates_12345',
+            accountName: 'Bill Gates',
+            bankName: 'Swiss Bank',
+            accountNumber: '23497344567867',
+            accountType: 'investment',
+            'SWIFT_BIC': '34575',
+            portfolio: 50000000000
+        } // _RI = 1
+    ],
+    cusListB: [
+        { uid: 'warren_buffet_12345',
+            accountName: 'Warren Buffet',
+            bankName: 'Swiss Bank',
+            accountNumber: '345546768789',
+            accountType: 'investment',
+            'SWIFT_BIC': '34575',
+            portfolio: 100000000000
+        } // _RI = 2
+    ]
+}
+
+exports.assignTWO = {
+    jobID: 'bank_2',
+    cusListA: [
+        {
+            uid: 'steve_jobs_12345',
+            accountName: 'Steve Jobbs',
+            bankName: 'ICBC',
+            accountNumber: '32456767876',
+            accountType: 'investment',
+            'SWIFT_BIC': '237687',
+            portfolio: 50000000000
+        }, // _RI = 3
+        {
+            uid: 'bill_gates_12345',
+            accountName: 'Bill Gates',
+            bankName: 'ICBC',
+            accountNumber: '3247890890',
+            accountType: 'investment',
+            'SWIFT_BIC': '4565',
+            portfolio: 90000000000
+        } // _RI = 4
+    ],
+    cusListB: [{ uid: `warren_buffet_12345`,
+        accountName: 'Warren Buffet',
+        bankName: 'ICBC',
+        accountNumber: '345567898074',
+        accountType: 'investment',
+        'SWIFT_BIC': '89456',
+        portfolio: 70000000000
+    }] // _RI = 5
+}
--- examples/app.async.example.js
+++ examples/app.async.example.js
@@ -26,72 +26,7 @@ const asyncData = (time = 2000, data) => {
         }, time)
     })
 }
-
-const assignONE = {
-    jobID: 'bank_1',
-    cusListA: [
-        {
-            uid: 'steve_jobs_12345',
-            accountName: 'Steve Jobbs',
-            bankName: 'Swiss Bank',
-            accountNumber: '2346547435',
-            accountType: 'investment',
-            'SWIFT_BIC': '34575',
-            portfolio: 20000000000
-        }, // _RI = 0
-        {
-            uid: 'bill_gates_12345',
-            accountName: 'Bill Gates',
-            bankName: 'Swiss Bank',
-            accountNumber: '23497344567867',
-            accountType: 'investment',
-            'SWIFT_BIC': '34575',
-            portfolio: 50000000000
-        } // _RI = 1
-    ],
-    cusListB: [
-        { uid: 'warren_buffet_12345',
-            accountName: 'Warren Buffet',
-            bankName: 'Swiss Bank',
-            accountNumber: '345546768789',
-            accountType: 'investment',
-            'SWIFT_BIC': '34575',
-            portfolio: 100000000000
-        } // _RI = 2
-    ]
-}
-
-const assignTWO = {
-    jobID: 'bank_2',
-    cusListA: [
-        {
-            uid: 'steve_jobs_12345',
-            accountName: 'Steve Jobbs',
-            bankName: 'ICBC',
-            accountNumber: '32456767876',
-            accountType: 'investment',
-            'SWIFT_BIC': '237687',
-            portfolio: 50000000000
-        }, // _RI = 3
-        {
-            uid: 'bill_gates_12345',
-            accountName: 'Bill Gates',
-            bankName: 'ICBC',
-            accountNumber: '3247890890',
-            accountType: 'investment',
-            'SWIFT_BIC': '4565',
-            portfolio: 90000000000
-        } // _RI = 4
-    ],
-    cusListB: [{ uid: `warren_buffet_12345`,
-        accountName: 'Warren Buffet',
-        bankName: 'ICBC',
-        accountNumber: '345567898074',
-        accountType: 'investment',
-        'SWIFT_BIC': '89456',
-        portfolio: 70000000000
-    }] // _RI = 5
-}
+const { assignONE, assignTWO } = require('./app.async.data')
 
 const BROKER = {
     fee: 10000,
@@ -127,7 +62,7 @@ prm.set(assignONE.cusListA, assignONE.jobID)
         // Warren Buffet account number
         if (v.dataSet.accountNumber === '345567898074') return true
     })
-    .markDone(assignTWO.jobID)
+    // .markDone(assignTWO.jobID) NOTE when set, can ignore future changes to this job
 // compute some logic based on previous selection
     .compute(async(d) => {
         var brokerData = await asyncData(1500, cloneDeep(BROKER))
